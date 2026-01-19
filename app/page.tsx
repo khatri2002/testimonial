805222833209b97278afe348bb5cf209cc68edd6
Jay Khatri <jaykhatri225@gmail.com>
2026-01-19 11:17:24 +0530

    chore(build): remove monorepo workaround and clean up scripts
    - Removed nextjs-monorepo-workaround-plugin
    - Removed webpack flags from dev and build scripts
    - Added dummy database fetch function on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <>Home</>;
+import { prisma } from "@/prisma";
+
+export default async function Home() {
+  const spaces = await prisma.space.findMany();
+
+  return spaces.length === 0
+    ? "no space found"
+    : `${spaces.length} space found`;
 }
